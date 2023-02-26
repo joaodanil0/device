ead725d0e0d274526a893db1fdb6494650188027
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-26 19:20:42 -0400

    Creating Device Model (mvc)

--- device/models.py
+++ device/models.py
@@ -3,8 +3,10 @@ from datetime import date
 
 # Create your models here.
 class Device(models.Model):
+    image = models.ImageField(upload_to='devices')
     model = models.CharField(max_length=30)
     name = models.CharField(max_length=30)    
     register_date = models.DateField(default=date.today)
     quantity = models.IntegerField()
     description = models.TextField()
+    
