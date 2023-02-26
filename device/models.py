31330ac4c717aa25f081eaa5186cc98721e19ebb
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-26 08:11:36 -0400

    Creating device model (mvc)

--- device/models.py
+++ device/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
+class Device(models.Model):
+    model = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)    
+    register_date = models.DateField(default=date.today)
+    quantity = models.IntegerField()
+    description = models.TextField()
