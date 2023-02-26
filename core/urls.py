f7651db2761cca7947d8f563313974708ed21369
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-26 19:49:32 -0400

    Adding image support on register device

--- core/urls.py
+++ core/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path("device/", include('device.urls')),
     path("", include('system.urls')),
 ]
+
+urlpatterns += \
+    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- device/views.py
+++ device/views.py
@@ -1,7 +1,14 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import redirect
+from device.forms import RegisterDevice
 
 # Create your views here.
 
 def register(request):
-    return HttpResponse("Hello")
+    if request.method == "POST":
+        form = RegisterDevice(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('/')
+        else:
+            return HttpResponse(form)
