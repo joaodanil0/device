1f2a04c127c832522fa8ae7e8f92faefd78b085f
joaodanil0 <joaodanilo1992@gmail.com>
2023-02-26 09:05:02 -0400

    Adding form to system view (mvc)

--- system/views.py
+++ system/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from device.forms import RegisterDevice
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    form = RegisterDevice()
+    return render(request, 'home.html', {"form":form})
